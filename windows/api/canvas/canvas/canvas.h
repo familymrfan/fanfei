6e014fcaa144275ec4d65f924a920436cb1a3e4e
fanfei <familymr.fan@gmail.com>
2013-07-30 12:02:15 +0800

    canvas add d2d

--- windows/api/canvas/canvas/canvas.h
+++ windows/api/canvas/canvas/canvas.h
@@ -3,61 +3,105 @@
 
 #include <windows.h>
 #include <string>
+#include <D2D1.h>
 
 class Canvas
 {
 public:
-	operator HDC () const {
-		return hdc_;
-	}
-
-	void DrawPie(int x,int y,int width,int height,int x1,int y1,int x2,int y2,COLORREF color = RGB(0,0,0)){
-		auto brush = ::CreateSolidBrush(color);
-		auto old_brush = ::SelectObject(hdc_,brush);
-		::Pie(hdc_,x,y,x + width,y + height,x1,y1,x2,y2);
-		::SelectObject(hdc_,old_brush);
-		::DeleteObject(brush);
-	}
-
-	void DrawRound(int x,int y,int width,int height,COLORREF color = RGB(0,0,0)){
-		auto brush = ::CreateSolidBrush(color);
-		auto old_brush = ::SelectObject(hdc_,brush);
-		::Ellipse(hdc_,x, y, x + width, y + height);
-		::SelectObject(hdc_,old_brush);
-		::DeleteObject(brush);
-	}
-
-	void DrawRect(int x,int y,int width,int height,COLORREF color = RGB(0,0,0)){
-		auto brush = ::CreateSolidBrush(color);
-		auto old_brush = ::SelectObject(hdc_,brush);
-		::Rectangle (hdc_, x, y, x + width, y + height);
-		::SelectObject(hdc_,old_brush);
-		::DeleteObject(brush);
-	}
-
-	void DrawText(std::wstring text,int x,int y){
-		::TextOut(hdc_,x,y,text.c_str(),text.size());
-	}
-protected:
-	Canvas(HDC hdc):hdc_(hdc){}
-private:
-	HDC hdc_;
+    virtual void DrawRect(int x,int y,int width,int height,COLORREF color = RGB(0,0,0)) = 0;
 };
 
 class PaintCanvas :public Canvas
 {
 public:
-	PaintCanvas(HWND hwnd)
-		:hwnd_(hwnd),Canvas(::BeginPaint(hwnd_,&ps_)){
+    PaintCanvas(HWND hwnd)
+        :hwnd_(hwnd) {
+        hdc_ = ::BeginPaint(hwnd_,&ps_);
+    }
 
-	}
+    virtual void DrawRect(int x,int y,int width,int height,COLORREF color = RGB(0,0,0)) override{
+        auto brush = ::CreateSolidBrush(color);
+        auto old_brush = ::SelectObject(hdc_,brush);
+        ::Rectangle (hdc_, x, y, x + width, y + height);
+        ::SelectObject(hdc_,old_brush);
+        ::DeleteObject(brush);
+    }
 
-	~PaintCanvas(){
-		::EndPaint(hwnd_,&ps_);
-	}
+    ~PaintCanvas(){
+        ::EndPaint(hwnd_,&ps_);
+    }
+private:
+    HDC hdc_;
+    PAINTSTRUCT ps_;
+    HWND hwnd_;
+};
+
+class D2DCanvas :public Canvas
+{
+public:
+    D2DCanvas(HWND hwnd)
+        :hwnd_(hwnd) {
+        CreateD2DTarget();
+        pRenderTarget->BeginDraw() ;
+        // Clear background color white
+        pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+    }
+
+    ~D2DCanvas(){
+        pRenderTarget->EndDraw() ;
+        pRenderTarget->Release();
+        pBlackBrush->Release();
+        pD2DFactory->Release();
+    }
+
+    virtual void DrawRect(int x,int y,int width,int height,COLORREF color = RGB(0,0,0)) override{
+        CreateD2DBrush();
+
+        pRenderTarget->DrawRectangle(
+                  D2D1::RectF(100.f, 100.f, 500.f, 500.f),
+                  pBlackBrush);
+    }
 protected:
-	PAINTSTRUCT ps_;
-	HWND hwnd_;
+    void CreateD2DTarget() {
+        HRESULT hr ;
+        hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pD2DFactory) ;
+        if (FAILED(hr))
+        {
+            MessageBox(hwnd_, L"Create D2D factory failed!", L"Error", 0) ;
+            return ;
+        }
+
+        RECT rc;
+        GetClientRect(hwnd_,&rc);
+        hr = pD2DFactory->CreateHwndRenderTarget(
+            D2D1::RenderTargetProperties(),
+            D2D1::HwndRenderTargetProperties(hwnd_, 
+            D2D1::SizeU(rc.right - rc.left,rc.bottom - rc.top)), 
+            &pRenderTarget);
+        if (FAILED(hr)) {
+            MessageBox(hwnd_, L"Create render target failed!", L"Error", 0) ;
+            return ;
+        }
+    }
+
+    void CreateD2DBrush() {
+        HRESULT hr ;
+        // Create a brush
+        hr = pRenderTarget->CreateSolidColorBrush(
+            D2D1::ColorF(D2D1::ColorF::Red),
+            &pBlackBrush
+                ) ;
+        if (FAILED(hr)) {
+            MessageBox(hwnd_, L"Create brush failed!", L"Error", 0) ;
+            return ;
+        }
+    }
+private:
+    ID2D1Factory* pD2DFactory;
+    ID2D1HwndRenderTarget* pRenderTarget;
+    ID2D1SolidColorBrush* pBlackBrush;
+
+    HWND hwnd_;
 };
 
 #endif // define CANVAS_H
--- windows/api/canvas/window/view.cpp
+++ windows/api/canvas/window/view.cpp
@@ -21,13 +21,6 @@ bool View::Generate(HWND hwnd,int x,int y,int cx,int cy){
 }
 
 void View::Paint(const Model& model){
-	PaintCanvas cv(model.GetHwnd());
-	
+	D2DCanvas cv(model.GetHwnd());
 	cv.DrawRect(10,10,400,400,RGB(100,30,200));
-	cv.DrawRound(20,20,200,200,RGB(200,130,100));
-	cv.DrawRound(30,30,100,100,RGB(100,130,100));
-	cv.DrawPie(30,30,100,100,
-		0,0,
-		30,0,RGB(130,200,100));
-	cv.DrawText(L"123",0,0);
 }
