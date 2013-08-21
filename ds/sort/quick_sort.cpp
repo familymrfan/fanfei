544ad55bf9b5551fdbdaca382128e978d590c639
fanfei <familymr.fan@gmail.com>
2013-08-21 22:12:34 +0800

    add quick sort to ds

---
+++ ds/sort/quick_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int a[] = {49, 38, 65, 97, 76, 13, 27};
+const unsigned int size = sizeof(a)/sizeof(int);
+
+void QSort(int low, int hign){
+    if(low > hign) {
+        return ;   
+    }
+    
+    int i = low, j = hign;
+    int x = a[i];
+    bool left = false;
+    while(i < j) {
+        if(left) {
+            if(x < a[i]) {
+                left = false;
+                a[j] = a[i];
+                j--;
+                continue;
+            }
+            i++;
+        } else {
+            if(x > a[j]) {
+                left = true;
+                a[i] = a[j];
+                i++;
+                continue;
+            }
+            j--;
+        }
+    }
+    a[i] = x;
+    QSort(low, i-1);
+    QSort(i+1, hign);
+}
+
+int main()
+{
+    QSort(0, size - 1);
+    for(unsigned int i = 0; i<size; i++) {
+     std::cout << a[i] << std::endl;
+    }
+   
+}
