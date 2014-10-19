a61f8b2960d1a4f1e3148aa866b8eeeefa80fd8d
fanfei <familymrfan@gmail.com>
2014-10-19 23:32:56 +0800

    加入左侧边栏

--- hippo/hippo/ViewController.m
+++ hippo/hippo/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 #import "DataBaseManager.h"
+#import "CDRTranslucentSideBar.h"
 
-@interface ViewController ()
+@interface ViewController () <CDRTranslucentSideBarDelegate>
+
+@property (nonatomic) CDRTranslucentSideBar *sideBar;
 
 @end
 
@@ -18,12 +21,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"view did load");
+    self.sideBar = [[CDRTranslucentSideBar alloc] init];
+    self.sideBar.sideBarWidth = 50;
+    self.sideBar.translucentAlpha = 0.2f;
+    self.sideBar.translucentTintColor = [UIColor blackColor];
+    self.sideBar.delegate = self;
+    self.sideBar.tag = 0;
+    
+    // Add PanGesture to Show SideBar by PanGesture
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
+    [self.view addGestureRecognizer:panGestureRecognizer];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Gesture Handler
+- (void)handlePanGesture:(UIPanGestureRecognizer *)recognizer {
+    self.sideBar.isCurrentPanGestureTarget = YES;
+    [self.sideBar handlePanGestureToShow:recognizer inView:self.view];
 }
 
 @end
